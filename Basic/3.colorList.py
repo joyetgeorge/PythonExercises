3760f2fa8007824b10f625c09dd7fc4934e4ee20
joyetgeorge <joyetgeorge07@gmail.com>
2021-05-19 19:54:13 +0530

    Update

--- Basic/3.colorList.py
+++ Basic/3.colorList.py
@@ -1,4 +1,4 @@
-""" Write a Python program to display the first and last colors from the following list. Go to the editor
+""" Write a Python program to display the first and last colors from the following list.
 color_list = ["Red","Green","White" ,"Black"] """
 
 color_list = ["Red","Green","White" ,"Black"]
